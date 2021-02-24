01a606ccb6e78f1f9dfa9e4767bcb022db9e353f
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2021-02-24 02:22:23 -0600

    Rock the vote part 2

---
+++ Week4/RockTheVote/routes/todoRouter.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const todoRouter = express.Router()
+const Todo = require("../models/todo")
+
+//Get all/Read
+todoRouter.get('/', (req, res, next) => {
+    Todo.find((err, todos) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(200).send(todos)
+    })
+  })
+
+  //Get one/Read by user id
+  todoRouter.get("/user", (req, res, next) => {
+    Todo.find({ user: req.user._id }, (err, todos) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(200).send(todos)
+    })
+  })
+
+  //Post/create
+  todoRouter.post("/", (req, res, next) => {
+    req.body.user = req.user._id
+    const newTodo = new Todo(req.body)
+    newTodo.save((err, savedTodo) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(savedTodo)
+    })
+  })
+
+//Put/update
+todoRouter.put("/:todoId", (req, res, next) => {
+    Todo.findOneAndUpdate(
+      { _id: req.params.todoId, user: req.user._id },
+      req.body,
+      { new: true },
+      (err, updatedTodo) => {
+        if(err){
+          res.status(500)
+          return next(err)
+        }
+        return res.status(201).send(updatedTodo)
+      }
+    )
+  })
+
+//Delete
+todoRouter.delete("/:todoId", (req, res, next) => {
+    Todo.findOneAndDelete(
+      { _id: req.params.todoId, user: req.user._id },
+      (err, deletedTodo) => {
+        if(err){
+          res.status(500)
+          return next(err)
+        }
+        return res.status(200).send(`Successfully delete todo: ${deletedTodo.issue}`)
+      }
+    )
+  })
+
+
+  module.exports = todoRouter
